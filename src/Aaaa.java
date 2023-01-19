65261fbb2fd0f759a1a8540dae3b275908187f79
Chris <chris@ChrisRuan2025.local>
2023-01-19 09:20:23 +0800

    Initial commit

---
+++ src/Aaaa.java
@@ -0,0 +1,44 @@
+public class Aaaa {
+    public static void main(String[] args) {
+//1
+int myInt = 10;
+//2
+int myInt2 = 4;
+//3
+double myDouble = 2.5;
+//4
+char myChar = 'A';
+//System.out.print(myInt/myDouble);
+        //System.out.print(myInt/myInt2);
+        int myInt3 = (int) myDouble;
+//System.out.print(myInt3);
+     //   System.out.print(12.0/0);
+        double myDouble2 = 4.6;
+        double myDouble3 = 4.4;
+        double myDouble4 = 4.5;
+myDouble2 += 0.5;
+        myDouble2 = (int) myDouble2;
+        //System.out.print(myDouble2);
+            myDouble3 += 0.5;
+            myDouble3 = (int) myDouble3;
+            //System.out.print(myDouble3);
+        myDouble4 += 0.5;
+        myDouble4 = (int) myDouble4;
+        //System.out.print(myDouble4);
+
+        int myChar2 = (int) myChar;
+        //System.out.print(myChar2);
+        char myChar3 = (char) myInt;
+               // System.out.print("hello"+myChar3+"goodbye");
+        int myint3 = 7;
+        //System.out.print(myint3++);
+        //System.out.print(myint3);
+        //System.out.print(++myint3);
+//System.out.print(145%10);
+        //System.out.print(178%10);
+        System.out.print(10%2);
+        System.out.print(11%2);
+        System.out.print(12%2);
+
+    }
+}
